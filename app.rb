e83f709a523d3922af84696df04552392f3c661e
Neznauy <alexandr.neznauy.shlyakov@outlook.com>
2021-06-06 08:54:31 +0200

    new offences

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative './config.rb'
 require_relative './lib/services.rb'
 
-s = Services::ServiceOne.new({name: 'name', value: 1}).tap(&:call)
+s = Services::ServiceOne.new({name: 'name', value: 1, env: Application.config.env}).tap(&:call)
 puts s.result
 
 s = Services::ServiceOne.new({name: nil, value: 1}).tap(&:call)
