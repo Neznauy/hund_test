fd5caec2ef5b74d16551e39a5e4753b15217e844
Neznauy <alexandr.neznauy.shlyakov@outlook.com>
2021-07-08 11:21:49 +0200

    new rails offenses

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -46,4 +46,32 @@ RSpec.describe Transaction, type: :model do
       end
     end                                          
   end
+
+  describe 'new offense' do
+    describe 'new offense nested' do
+      describe 'more new offense nested' do
+        context 'when nested context' do
+          context 'when more nested context' do
+            let(:transaction) { Transaction.new }
+            let!(:excess_let_1) { 1 }
+            let!(:excess_let_2) { 2 }
+            let(:excess_let_3) { 3 }
+            let(:excess_let_4) { 4 }
+            let(:excess_let_5) { 5 }
+            let(:excess_let_6) { 6 }
+            let(:excess_let_7) { 7 }
+            let(:excess_let_8) { 8 }
+            let(:excess_let_9) { 9 }
+
+            before { @bar = 2 }
+
+            context 'when new offense presents' do
+              it { expect(transaction.valid?).to be_falsey }
+              it { expect(@foo).to eq 2 }
+            end
+          end
+        end
+      end
+    end                                          
+  end  
 end
